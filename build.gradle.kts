f733c675263061aabb27988c523defda7c3df1ae
YJGwon <trulyforky@gmail.com>
2023-08-08 22:00:51 +0900

    chore: initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.1.2"
+    id("io.spring.dependency-management") version "1.1.2"
+    id("org.asciidoctor.jvm.convert") version "3.3.2"
+}
+
+group = "com.wanted"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // spring web
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // spring data jpa
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // spring validation
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // flyway
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-mysql")
+
+    // lombok
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    // h2 database
+    runtimeOnly("com.h2database:h2")
+
+    // mysql
+    runtimeOnly("com.mysql:mysql-connector-j")
+
+    // springdoc openapi (swagger ui)
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
+
+    // spring test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // rest-assured
+    testImplementation("io.rest-assured:rest-assured:5.3.0")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
