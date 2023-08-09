e5a15bad597fe4bd404d7d1d4342cbf2614a25c4
YJGwon <trulyforky@gmail.com>
2023-08-09 21:21:05 +0900

    feat(member): 이메일로 회원 정보 조회

--- src/main/java/com/wanted/preonboarding/member/repository/MemberRepository.java
+++ src/main/java/com/wanted/preonboarding/member/repository/MemberRepository.java
@@ -1,9 +1,13 @@
 package com.wanted.preonboarding.member.repository;
 
+import com.wanted.preonboarding.member.domain.Email;
 import com.wanted.preonboarding.member.domain.Member;
+import java.util.Optional;
 import org.springframework.data.repository.Repository;
 
 public interface MemberRepository extends Repository<Member, Long> {
 
     Member save(Member member);
+
+    Optional<Member> findByEmail(Email email);
 }
--- src/test/java/com/wanted/preonboarding/member/repository/MemberRepositoryTest.java
+++ src/test/java/com/wanted/preonboarding/member/repository/MemberRepositoryTest.java
@@ -2,11 +2,13 @@ package com.wanted.preonboarding.member.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.wanted.preonboarding.member.domain.Email;
 import com.wanted.preonboarding.member.domain.Member;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
 
 @DataJpaTest
 class MemberRepositoryTest {
@@ -25,5 +27,21 @@ class MemberRepositoryTest {
 
         //then
         assertThat(savedMember.getId()).isNotNull();
+    }
+
+    @DisplayName("이메일로 회원 정보를 조회한다.")
+    @Test
+    void findByEmail() {
+        // given
+        final String rawEmail = "test@test.com";
+        final Member expected = Member.ofNew(rawEmail, "test1234");
+        memberRepository.save(expected);
+
+        // when
+        final Email email = new Email(rawEmail);
+        final Member found = memberRepository.findByEmail(email).get();
+
+        // then
+        assertThat(found.getId()).isEqualTo(expected.getId());
     }
 }
