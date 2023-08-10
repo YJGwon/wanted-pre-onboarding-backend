02d9422938126ebad4c94d5a817b9a032af58b02
YJGwon <trulyforky@gmail.com>
2023-08-10 16:55:55 +0900

    feat(post): 게시글 목록 DB 조회 service 구현

--- src/main/java/com/wanted/preonboarding/post/repository/PostRepository.java
+++ src/main/java/com/wanted/preonboarding/post/repository/PostRepository.java
@@ -1,9 +1,13 @@
 package com.wanted.preonboarding.post.repository;
 
 import com.wanted.preonboarding.post.domain.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.Repository;
 
 public interface PostRepository extends Repository<Post, Long> {
 
     Post save(Post post);
+
+    Page<Post> findAll(Pageable pageable);
 }
