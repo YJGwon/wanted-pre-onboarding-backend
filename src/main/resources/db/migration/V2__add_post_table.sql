dab024b35e4ecb6dd4b40128f402b80afc08a1be
YJGwon <trulyforky@gmail.com>
2023-08-11 20:32:26 +0900

    fix(db): 게시글 내용 column type 수정

--- src/main/resources/db/migration/V2__add_post_table.sql
+++ src/main/resources/db/migration/V2__add_post_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE post (
     post_id BIGINT(20) NOT NULL AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
-    content VARCHAR(16383) NOT NULL,
+    content TEXT NOT NULL,
     writer_id BIGINT(20) NOT NULL,
     PRIMARY KEY (post_id),
     FOREIGN KEY (writer_id) REFERENCES member (member_id)
